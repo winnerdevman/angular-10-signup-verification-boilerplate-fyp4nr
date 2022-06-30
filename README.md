ba7008f81a6e105f4d0a7f639e155109e90c7d17
winnerdevman <winwilust@gmail.com>
2022-06-30 05:55:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-10-signup-verification-boilerplate-fyp4nr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-10-signup-verification-boilerplate-fyp4nr)
---
+++ src/app/profile/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { AccountService } from '../_services';
+
+@Component({ templateUrl: 'details.component.html' })
+export class DetailsComponent {
+    account = this.accountService.accountValue;
+
+    constructor(private accountService: AccountService) { }
+}
